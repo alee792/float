4c98298b59913753bbbc9f2266a32005e2906122
alee792 <alee792@gmail.com>
2017-10-19 18:44:38 -0400

    got handlebars up

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 // Load mongoose package
 const mongoose = require('mongoose');
 
+// Load and register handlebars
+const eng = require('express-handlebars');
+
 const express = require('express');
 const config = require('./config');
 const router = require('./routes');
@@ -16,10 +19,19 @@ mongoose.connection.openUri(`mongodb://${config.db.host}/${config.db.dbName}`);
 require('./models/user.js');
 
 const app = express();
+
+// Register handlebars as engine
+app.engine('handlebars', eng({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 app.use('/', router);
+
+app.get('*', (req, res, next) =>{
+  res.render('error');
+});
 
 
 app.listen(config.port, function() {
