86dd68989be9f02ebf86df3bdc71df36ae1542dc
Anthony Lee <alee792@gmail.com>
2017-10-19 11:05:16 -0400

    Made an error padge and added a bunch of junk

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mustacheExpress = require('mustache-express');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+const config = require('./config');
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-app.engine('mustache', mustacheExpress);
+app.set('views', path.join(__dirname, 'views/templates'));
+app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 
 // uncomment after placing your favicon in /public
@@ -43,6 +45,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(config.port, function() {
+  console.log(`${config.appName} is listening on port ${config.port}`);
 });
 
 module.exports = app;
