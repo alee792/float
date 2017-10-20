b039b10475a8e3816b29d84bef3ca9066b0916b4
Anthony Lee <alee792@gmail.com>
2017-10-20 15:15:49 -0400

    Pull in sessions

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 // src/server.js
 const path = require('path');
-const bodyParser = require('body-parser');
-
-// Load mongoose package
-const mongoose = require('mongoose');
-
-// Load and register handlebars
-const eng = require('express-handlebars');
-
 const express = require('express');
 const config = require('./config');
 const router = require('./routes');
 
+// app starts here!
+const app = express();
+
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const eng = require('express-handlebars');
+
 // Connect to MongoDB and create/use database as configured
 mongoose.connection.openUri(`mongodb://${config.db.host}/${config.db.dbName}`);
 
-// Import all models
-require('./models/user.js');
-
-const app = express();
+// Use sessions
+app.use(session({
+  secret: 'insecure',
+  resave: true,
+  saveUninitialized: false
+}));
 
 // Register handlebars as engine
 app.engine('handlebars', eng({defaultLayout: 'main'}));
