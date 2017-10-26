de0907e167d0e8aefdfe951170b41c4b9b2d778b
Anthony Lee <alee792@gmail.com>
2017-10-26 18:30:50 -0400

    start feedback and post models

---
+++ models/feedback.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+var FeedbackSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    post_id: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    created_by: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+        // require: true if float is false?
+    },
+    float: {
+        type: Boolean,
+        required: true,
+    },
+    flags: {
+        flag: {
+            type: text,
+        }
+    },
+    tags: {
+        tag: {
+            type: text,
+        }
+    },
+    // Create proposed post object created by reviewers
+})
--- models/post.js
+++ models/post.js
@@ -1,18 +1,10 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var PostUserSchmea = new mongoose.Schema({
-    name: {
-        type: String
-    },
-    created_dttm: {
-        type: Date,
-        default: Date.now
-    },
-    primary_twitter: {
-        type: String,
-        unique: true
-    }
-})
+// This object contains:
+// 1. Metadata related to the post
+// 2. The content of the proposed post
+// 3. A plan of when the post will occur, 
+//    and what account will post it
 
 var PostSchema = new mongoose.Schema({
     id: {
@@ -21,19 +13,68 @@ var PostSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
+    // When the post-candidate is created
     created_at: {
         type: Date,
-        default: Date.now
+        required: true,
+        default: Date.now,
     },
-    text: {
-        type: String, 
-        required: true
-    },
-    source: {
-        type: String
-    },
-    user: {
+    // Who wrote the post-candidate
+    created_by: {
         type: String,
-        unique: true
-    }
+        required: true,
+    },
+    // Flagged for review!!
+    flags: {
+        flag: {
+            type: Text,
+        }
+    },
+    // Flagged for review!!
+    tags: {
+        tag: {
+            type: Text,
+        }
+    },
+    // If true, post plan will proceed 
+    floated: {
+        type: Boolean,
+        default: false
+    },
+    // Object to hold post-candidate info
+    postCandidate: {
+        // Content of the post-candidate
+        content: {
+            type: String,
+            required: true
+        },
+        // Any urls that may be in the post
+        urls: {
+            type: String,
+            // Parse string for URL and validate it
+        },
+        hashtags: {
+            type: String,
+            // Parse string for hastags and validate it
+        },
+        // The intended audience of the post
+        audience: {
+            type: String,
+        },
+        //Any other additional context
+        context: {
+            type: String,
+        },
+    },
+    post_plan: {
+        // The account that will be posting
+        post_account: {
+            type: String,
+            required: true,
+        },
+        // When the account is scheduled to post
+        post_time: {
+            type: Date,
+        },
+    },
 });
