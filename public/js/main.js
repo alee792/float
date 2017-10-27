d62d89a98c95ae1778fc86ebb6a20fe2bc794516
alee <alee792@gmail.com>
2017-10-27 10:31:04 -0400

    Auto-fit image complete! Looks good.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
-$(document).ready( () => {    
-    $('#profile-toggle').click( () => {
+$(document).ready(() => {
+    $('#profile-toggle').click(() => {
         $('#profile').hide(750);
-        });
+    });
+
+    // Toggle CSS for card-media
+    if ($('.card-media').prop('naturalWidth') > $('.card-media').prop('naturalHeight')) {
+        $('.card-media').css('height', 'auto');
+    } else {
+        $('.card-media').css('width', 'auto')
+    }
 });
